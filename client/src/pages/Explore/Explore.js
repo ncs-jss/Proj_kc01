edb4f547571a8c05c8ce95b50c89730d960cae85
geekayush <srivastavs61@gmail.com>
2018-08-06 10:47:27 +0530

    Resolves #34 : Map data to backend

--- client/src/pages/Explore/Explore.js
+++ client/src/pages/Explore/Explore.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Explore.css";
 import Maps from "../../components/Maps/Maps";
 
-const Explore = () => (
+const Explore = props => (
   <div className="explore">
     <div className="explore-content">
       <div className="mapContainer">
-        <Maps />
+        <Maps data={props.data} />
       </div>
     </div>
   </div>
