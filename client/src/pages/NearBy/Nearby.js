41cd2d86906639a5de5d8a270b9396868c2a47df
geekayush <srivastavs61@gmail.com>
2018-08-24 03:41:10 +0530

    Resolves #65 : Modify masonry layout

--- client/src/pages/NearBy/Nearby.js
+++ client/src/pages/NearBy/Nearby.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Nearby.css";
+import Masonry from "react-masonry-component";
 import Card from "../../components/Card/Card";
 
 const Nearby = props => (
   <div className="nearby" id="container">
     <div className="nearby-content" id="myContent">
-      {props.data.map(p => (
-        <Card
-          name={p.name}
-          dist={p.dist}
-          type={p.type}
-          desc={p.desc}
-          link={p.link}
-          exp={p.expense}
-          key={p._id}
-        />
-      ))}
+      <Masonry>
+        {props.data.map(p => (
+          <Card
+            name={p.name}
+            dist={p.dist}
+            type={p.type}
+            desc={p.desc}
+            link={p.link}
+            exp={p.expense}
+            key={p._id}
+          />
+        ))}
+      </Masonry>
     </div>
   </div>
 );
--- client/src/pages/Societies/Societies.js
+++ client/src/pages/Societies/Societies.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Societies.css";
+import Masonry from "react-masonry-component";
 import Card from "../../components/Card/Card";
 
 const Societies = props => (
   <div className="societies" id="container">
     <div className="societies-content" id="myContent">
-      {props.data.map(p => (
-        <Card
-          name={p.name}
-          dist={p.dist}
-          type={p.type}
-          desc={p.desc}
-          link={p.link}
-          key={p._id}
-        />
-      ))}
+      <Masonry>
+        {props.data.map(p => (
+          <Card
+            name={p.name}
+            dist={p.dist}
+            type={p.type}
+            desc={p.desc}
+            link={p.link}
+            key={p._id}
+          />
+        ))}
+      </Masonry>
     </div>
   </div>
 );
