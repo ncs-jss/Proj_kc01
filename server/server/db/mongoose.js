81a7b4226a4107c63ce071544ea0a76957e3132c
Vasu Mehra <vasumail007@gmail.com>
2019-05-02 01:04:59 +0530

    updated db connect

--- server/server/db/mongoose.js
+++ server/server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://ncsprojkc01:kanpur123@ds235251.mlab.com:35251/projkc01' || 'mongodb://localhost:27017/Nearby');
+
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
