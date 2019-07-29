030f6f9db33eaaff43d8632ae75f5c496c1da7b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-29 22:46:33 +0000

    Deploy ncs-jss/Proj_kc01 to github.com/ncs-jss/Proj_kc01.git:gh-pages

---
+++ precache-manifest.f89ad93d52b934c487da79b16b4da643.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6db2d62bc463fa64fcc0efbc9197100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb19651d40415dcb38d",
+    "url": "/static/css/2.b4600f21.chunk.css"
+  },
+  {
+    "revision": "022c29ddc51598b70383",
+    "url": "/static/css/main.b2017dee.chunk.css"
+  },
+  {
+    "revision": "edb19651d40415dcb38d",
+    "url": "/static/js/2.17ac3955.chunk.js"
+  },
+  {
+    "revision": "022c29ddc51598b70383",
+    "url": "/static/js/main.4dee01b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "460d5aab9a3747a2adb7fc7df28321e0",
+    "url": "/static/media/GT-Walsheim-Medium.460d5aab.woff"
+  },
+  {
+    "revision": "bae3a66a581b89e8619883496bbca0bd",
+    "url": "/static/media/GT-Walsheim-Medium.bae3a66a.eot"
+  },
+  {
+    "revision": "ef635e286addd1a62110811d4a2b796d",
+    "url": "/static/media/GT-Walsheim-Medium.ef635e28.ttf"
+  },
+  {
+    "revision": "0b9a20ebe1297d5500479ca7258e94c0",
+    "url": "/static/media/GT-Walsheim-Regular.0b9a20eb.woff"
+  },
+  {
+    "revision": "628f7108e18d0eb18bcec47fc8a9fb54",
+    "url": "/static/media/GT-Walsheim-Regular.628f7108.ttf"
+  },
+  {
+    "revision": "7b7a7736aef965648fdd8ca877a7d4b6",
+    "url": "/static/media/GT-Walsheim-Regular.7b7a7736.eot"
+  }
+]);
